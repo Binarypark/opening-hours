067023726f2268f327145e332891e18b35617fab
CosminPaun <cpaun88@gmail.com>
2014-08-28 23:26:15 +0200

    up

--- mail.php
+++ mail.php
@@ -1,11 +1,11 @@
 <?php
     
-  if (isset($_POST["email"])) {
-    $from = $_POST["email"]; // sender
-    $name = $_POST["name"];
-    $message = $_POST["message"];
+  if (isset($_GET["email"])) {
+    $from = $_GET["email"]; // sender
+    $name = $_GET["name"];
+    $message = $_GET["message"];
     // message lines should not exceed 70 characters (PHP rule), so wrap it
-    $message = wordwrap($message, 70);
+    $message = wordwrap($message, 170);
     // send mail
     mail("cpaun88@gmail.com",$name,$message,"From: $from\n");
     echo "Thank you for sending us feedback";
